1622505495e1974fccb32ff35d9e579d887d9f5a
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2024-03-26 05:51:19 -0700

    var

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    """
+    Returns a set of common elements in two sets.
+
+    Args:
+        set_1: The first set.
+        set_2: The second set.
+
+    Returns:
+        A set containing common elements in both sets.
+    """
+    return set_1.intersection(set_2)
