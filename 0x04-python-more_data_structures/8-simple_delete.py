a3a53ea7bd08e5fbbc231eacf84dbb60ae5f0a76
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2024-03-26 06:03:14 -0700

    var

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def simple_delete(a_dictionary, key=""):
+    """
+    Deletes a key in a dictionary.
+
+    Args:
+        a_dictionary: The input dictionary.
+        key: The key to delete.
+
+    Returns:
+        The updated dictionary.
+    """
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
