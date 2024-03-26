5d59d63f61378ead7d12e9be2dba5cdd4a9df36e
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2024-03-26 04:20:56 -0700

    var

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    """Adds two tuples."""
+    # If a tuple is smaller than 2, use the value 0 for each missing integer
+    if len(tuple_a) < 2:
+        tuple_a += (0, )
+    if len(tuple_b) < 2:
+        tuple_b += (0, )
+
+    # If a tuple is bigger than 2, use only the first 2 integers
+    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return result
+
+
+if __name__ == "__main__":
+    tuple_a = (1, 89)
+    tuple_b = (88, 11)
+    new_tuple = add_tuple(tuple_a, tuple_b)
+    print(new_tuple)
+
+    print(add_tuple(tuple_a, (1, )))
+    print(add_tuple(tuple_a, ()))
