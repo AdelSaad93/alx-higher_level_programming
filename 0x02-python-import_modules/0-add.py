c23bb1b9717a2b92bda68be02f91d60ce8eca2ee
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2024-03-26 02:47:31 -0700

    var

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     from add_0 import add
 
-    def fake_add(a, b):
-        return a - b
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, fake_add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
