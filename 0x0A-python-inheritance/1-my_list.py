9ad11adfdb8f916eed4eff5202b7ef7764bf440c
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2024-04-29 00:05:22 -0700

    var

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,18 +1,10 @@
-#!/usr/bin/python3
-"""
-Defines MyList that inherits from list
-"""
+i#!/usr/bin/python3
+"""Defines an inherited list class MyList."""
 
 
 class MyList(list):
-    """
-        This class inherits from list.
-        Attributes:
-        Methods:
-            print_sorted - prints the list in ascending order
-    """
+    """Implements sorted printing for the built-in list class."""
+
     def print_sorted(self):
-        """
-           prints a list in ascending order.
-        """
+        """Print a list in sorted ascending order."""
         print(sorted(self))
