9314b9e6d85dfc5d5bad32e80c8bf96c0414ad32
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2024-04-28 23:10:17 -0700

    var

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,22 @@
+i#!/usr/bin/python3
+"""Defines a class MyList that inherits from list."""
+
+
+class MyList(list):
+    """A custom list class."""
+
+    def print_sorted(self):
+        """Print the list sorted in ascending order."""
+        print(sorted(self))
+
+
+if __name__ == "__main__":
+    my_list = MyList()
+    my_list.append(1)
+    my_list.append(4)
+    my_list.append(2)
+    my_list.append(3)
+    my_list.append(5)
+    print(my_list)
+    my_list.print_sorted()
+    print(my_list)
