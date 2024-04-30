e570ac314f81c04e42fa35de5d68a36dd0bbd850
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2024-04-29 22:52:19 -0700

    var

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,18 @@
+class Rectangle(BaseGeometry):
+    """Rectangle class, inherits from BaseGeometry."""
+
+    def __init__(self, width, height):
+        """Initialize the Rectangle instance."""
+        super().__init__()
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """Return string representation of the Rectangle."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        """Return the area of the Rectangle."""
+        return self.__width * self.__height
