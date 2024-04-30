a46ba4b914512a22d144167d72be5c4e47fee3b7
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2024-04-29 22:00:26 -0700

    var

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""Defines a function to check if an object is an instance of a specified class."""
+
 
 def is_same_class(obj, a_class):
-    """Check if an object is an instance of a specified class."""
+    """Check if an object is an instance of a specified class.
+
+    Args:
+        obj (object): The object to check.
+        a_class (type): The class to compare against.
+
+    Returns:
+        bool: True if the object is an instance of the specified class;
+            otherwise False.
+    """
     return type(obj) is a_class
