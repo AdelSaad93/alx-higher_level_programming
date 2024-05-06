0bdf660e606bcf2728d53c249710d701ea09cbe5
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2024-05-05 19:54:20 -0700

    var

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
-"""Adds all arguments to a Python list and saves them to a file."""
+"""Add all arguments to a Python list and save them to a file."""
+
 
 import sys
-from os import path
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
-
-def add_items_to_list_and_save():
-    """
-    Adds all command line arguments to a Python list and saves them to a file.
-    The list is saved as a JSON representation in a file named add_item.json.
-    If the file doesn’t exist, it is created.
-    """
-    filename = "add_item.json"
-
-    if path.exists(filename):
-        my_list = load_from_json_file(filename)
-    else:
-        my_list = []
-
-    my_list.extend(sys.argv[1:])
-
-    save_to_json_file(my_list, filename)
 
 if __name__ == "__main__":
-    add_items_to_list_and_save()
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
