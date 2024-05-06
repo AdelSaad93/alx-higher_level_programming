371127dd84e90b77508e31e269d5b88ab70c31f5
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2024-05-05 20:41:02 -0700

    var

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,33 @@
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing Pascal's triangle of n.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]  # first element of each row is always 1
+        prev_row = triangle[i - 1]
+
+        for j in range(1, i):
+            # Each element (except the first and last) is the sum of the
+            # corresponding elements of the previous row
+            row.append(prev_row[j - 1] + prev_row[j])
+
+        row.append(1)  # last element of each row is always 1
+        triangle.append(row)
+
+    return triangle
+
+# Test the function
+if __name__ == "__main__":
+    def print_triangle(triangle):
+        """
+        Print the triangle
+        """
+        for row in triangle:
+            print("[{}]".format(",".join([str(x) for x in row])))
+
+    print_triangle(pascal_triangle(5))
