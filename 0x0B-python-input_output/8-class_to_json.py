38c0d3864dd4fe4af7c7d66c006102ae12ceda0a
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2024-05-05 20:11:18 -0700

    var

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,21 +1,7 @@
 #!/usr/bin/python3
+"""Defines a Python class-to-JSON function."""
 
 
 def class_to_json(obj):
-    """Returns the dictionary description with simple data structure
-    (list, dictionary, string, integer, and boolean) for JSON serialization
-    of an object.
-
-    Args:
-        obj: An instance of a Class.
-
-    Returns:
-        dict: A dictionary containing the serialized attributes of the object.
-    """
-    json_dict = {}
-    for attr_name in dir(obj):
-        if not attr_name.startswith("__") and not callable(getattr(obj, attr_name)):
-            attr_value = getattr(obj, attr_name)
-            if isinstance(attr_value, (list, dict, str, int, bool)):
-                json_dict[attr_name] = attr_value
-    return json_dict
+    """Return the dictionary representation of a simple data structure."""
+    return obj.__dict__
