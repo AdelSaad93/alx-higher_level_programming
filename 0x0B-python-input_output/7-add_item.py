2a474f967ebf2fea7ec838cecea2e7008c62fffd
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2024-05-05 19:52:26 -0700

    var

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -14,17 +14,13 @@ def add_items_to_list_and_save():
     """
     filename = "add_item.json"
 
-    # Check if the file exists
     if path.exists(filename):
-        # Load existing list from file
         my_list = load_from_json_file(filename)
     else:
         my_list = []
 
-    # Append command line arguments to the list
     my_list.extend(sys.argv[1:])
 
-    # Save the updated list to the file
     save_to_json_file(my_list, filename)
 
 if __name__ == "__main__":
