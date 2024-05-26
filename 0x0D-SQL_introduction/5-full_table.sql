d66b3f59956ac3514d1816f13776a67227751be4
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2024-05-25 23:09:38 -0700

    var

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,10 +1,6 @@
 -- Task 5: Print the full description of the table first_table
 -- This script prints the full description of the table first_table from the database hbtn_0c_0.
 
-first_table CREATE TABLE `first_table` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(128) DEFAULT NULL,
-  `c` char(1) DEFAULT NULL,
-  `created_at` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+SELECT TABLE_NAME, CREATE_TABLE 
+FROM information_schema.tables 
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
