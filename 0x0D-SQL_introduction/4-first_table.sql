45c5c7f17f15b7411b7d20c5966a7e80b71d83db
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2024-05-25 23:00:22 -0700

    var

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Task 4: Create a table called first_table
+-- This script creates a table called first_table in the current database.
+
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
