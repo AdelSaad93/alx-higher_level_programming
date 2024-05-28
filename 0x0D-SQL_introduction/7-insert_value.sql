5066039ac1f04b10e71c96ac0f4cad3206cac58a
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2024-05-27 21:07:55 -0700

    var

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- Task 7: Insert a new row in the table first_table
+-- This script inserts a new row into the table first_table in the database hbtn_0c_0.
+
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
