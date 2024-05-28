ed133f540d41bc0fbaf901ad6761f7f97b77a4fc
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2024-05-27 22:38:45 -0700

    var

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,14 +1,18 @@
 -- Task 0: My privileges!
 -- This script lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
 
--- Create user_0d_1 if it doesn't exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+-- Check if user_0d_1 exists and show grants if exists
+SELECT 
+    CASE
+        WHEN EXISTS (SELECT 1 FROM mysql.user WHERE User = 'user_0d_1' AND Host = 'localhost')
+        THEN SHOW GRANTS FOR 'user_0d_1'@'localhost'
+        ELSE 'User user_0d_1 does not exist'
+    END;
 
--- Create user_0d_2 if it doesn't exist
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Show privileges for user_0d_1
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- Show privileges for user_0d_2
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Check if user_0d_2 exists and show grants if exists
+SELECT 
+    CASE
+        WHEN EXISTS (SELECT 1 FROM mysql.user WHERE User = 'user_0d_2' AND Host = 'localhost')
+        THEN SHOW GRANTS FOR 'user_0d_2'@'localhost'
+        ELSE 'User user_0d_2 does not exist'
+    END;
