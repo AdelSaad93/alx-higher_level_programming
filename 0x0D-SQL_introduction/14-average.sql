0ee53aab57dd23e8c0fbc157aa1a5ff53abc9307
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2024-05-27 21:57:29 -0700

    var

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,5 @@
+-- Task 14: Compute the average score
+-- This script computes the average score of all records in the table second_table of the database hbtn_0c_0.
+-- The result column name should be average.
+
+SELECT AVG(score) AS average FROM second_table;
