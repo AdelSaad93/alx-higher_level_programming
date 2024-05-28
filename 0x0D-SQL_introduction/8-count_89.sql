0d7143f7acd129ed3eaa6c850c9370fa9ae3d53c
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2024-05-27 21:09:19 -0700

    var

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- Task 8: Count records with id = 89 in the table first_table
+-- This script counts the number of records with id = 89 in the table first_table from the database hbtn_0c_0.
+
+SELECT COUNT(*) FROM first_table WHERE id = 89;
