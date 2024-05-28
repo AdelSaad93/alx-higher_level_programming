f065e6b73fe091570a4e0bf90d8bc818e562c502
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2024-05-27 21:55:41 -0700

    var

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,4 @@
+-- Task 13: Remove records with score <= 5
+-- This script removes all records with a score less than or equal to 5 from the table second_table in the database hbtn_0c_0.
+
+DELETE FROM second_table WHERE score <= 5;
